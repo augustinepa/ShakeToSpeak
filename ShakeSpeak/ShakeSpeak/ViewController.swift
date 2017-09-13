f9bf6f120892def5ae48a18c61e057c5fb82f483
Augustine <augustinepa@gmail.com>
2017-09-13 11:11:50 +0530

    Initial Commit

---
+++ ShakeSpeak/ShakeSpeak/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShakeSpeak
+//
+//  Created by A, Augustine on 9/13/17.
+//  Copyright © 2017 A, Augustine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
